fed77671be0274e6b861ab4aff1ea42c9eb9145d
Tsnissanka <42473077+Tsnissanka@users.noreply.github.com>
2019-04-16 20:15:14 -0500

    got lines to be drawn in fractal tree lab

--- FractalTreeLab/FractalTree.java
+++ FractalTreeLab/FractalTree.java
@@ -2,6 +2,7 @@ import java.awt.*;
 import javax.swing.JFrame;
 import java.awt.geom.Line2D;
 import java.awt.geom.Point2D;
+import javax.swing.JComponent;
 /**
  * Write a description of class FractalTree here.
  *
@@ -9,38 +10,46 @@ import java.awt.geom.Point2D;
  * @version (a version number or a date)
  */
 
-public class FractalTree
+public class FractalTree extends JComponent
 {
-    private final int BRANCHANGLE = 15;
-    private final double BRANCHSUBTRACTION = 10;
+    private final int BRANCHANGLE = 100;
     private int order;
     public FractalTree(){
         JFrame frame = new JFrame();
         frame.setTitle("Fractal Tree");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(800, 800);
+        this.setPreferredSize(new Dimension(800,800));
         frame.setVisible(true);
-        
-        
-       
+        frame.add(this);
+        frame.pack();
     }
     
-    public void drawFractal(int orderNum, double x1, double y1, double x2, double y2, Graphics g){
-        order = orderNum;
-        if (order == 0){
+    public void drawFractal(int orderNum, double x1, double y1, double x2, double y2, Graphics2D g2, double degrees){
+        if (orderNum == 0){
             return;
         }
-        Graphics2D g2 = (Graphics2D) g;
         g2.draw(new Line2D.Double(x1, y1, x2, y2));
+        Point2D.Double line1 = new Point2D.Double(x1, y1);
+        Point2D.Double line2 = new Point2D.Double(x2, y2);
         
+        double len = (int) (.9 * line1.distance(line2));
         
+        double x3 = Math.cos(Math.toRadians(degrees)) * len + x2;
+        double y3 = -Math.sin(Math.toRadians(degrees)) * len + y2;
+        
+        double x4 = x2 - Math.cos(Math.toRadians(degrees)) * len;
+        double y4 = y2 - Math.sin(Math.toRadians(degrees)) * len;
+        
+        drawFractal(orderNum-1, x2, y2, x3, y3, g2, degrees - 15);
+        drawFractal(orderNum-1, x2, y2, x4, y4, g2, degrees + 15);
     }
     
     public void paintComponent(Graphics g){
+        g.setColor(Color.BLUE);
         Graphics2D g2 = (Graphics2D) g;
-        g2.setColor(Color.BLUE);
-        drawFractal(1, 400, 600, 400, 300, g2);
+        drawFractal(2, 400, 600, 400, 450, g2, BRANCHANGLE);
     }
+    
     public static void main(String[] args){
         FractalTree frame = new FractalTree();
     }
